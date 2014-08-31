05c98844e1a3aa33698b70437a517ae8e4e236c4
kenwilcox <wilcoxk@gmail.com>
2014-08-30 22:31:39 -0600

    Added Tip Calculator

--- SwiftTutorial.playground/section-1.swift
+++ SwiftTutorial.playground/section-1.swift
@@ -22,3 +22,51 @@ if onSaleInferred {
 }
 
 // Done with variables - lets play with classes!
+
+class TipCalculator {
+  let total: Double
+  let taxPct: Double
+  let subtotal: Double
+  
+  init(total: Double, taxPct: Double) {
+    self.total = total
+    self.taxPct = taxPct
+    subtotal = total / (taxPct + 1)
+  }
+  
+  func calcTipWithTipPct(tipPct: Double) -> Double {
+    return subtotal * tipPct
+  }
+  
+//  func printPossibleTips() {
+//    //println("15%: \(calcTipWithTipPct(0.15))")
+//    //println("18%: \(calcTipWithTipPct(0.18))")
+//    //println("20%: \(calcTipWithTipPct(0.20))")
+//    
+//    let possibleTipsInferred = [0.15, 0.18, 0.20]
+//    //let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
+//    //for possibleTip in possibleTipsInferred {
+//    //  println("\(possibleTip*100)%: \(calcTipWithTipPct(possibleTip))")
+//    //}
+//    // Or you can do loops like this
+//    for i in 0..<possibleTipsInferred.count {
+//      let possibleTip = possibleTipsInferred[i]
+//      println("\(possibleTip*100)%: \(calcTipWithTipPct(possibleTip))")
+//    }
+//  }
+  
+  func returnPossibleTips() -> [Int: Double] {
+    let possibleTipsInferred = [0.15, 0.18, 0.20]
+    //let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
+    
+    var retval = Dictionary<Int, Double>()
+    for possibleTip in possibleTipsInferred {
+      let intPct = Int(possibleTip * 100)
+      retval[intPct] = calcTipWithTipPct(possibleTip)
+    }
+    return retval
+  }
+}
+
+let tipCalc = TipCalculator(total: 33.25, taxPct: 0.06)
+tipCalc.returnPossibleTips()
