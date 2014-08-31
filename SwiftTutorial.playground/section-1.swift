c6eaf46f4c4d5def372bfa3c25ac6e7fb7d7ab2b
kenwilcox <wilcoxk@gmail.com>
2014-08-30 22:11:42 -0600

    More Variable Basics

--- SwiftTutorial.playground/section-1.swift
+++ SwiftTutorial.playground/section-1.swift
@@ -5,3 +5,20 @@ let editorialTeam = 23
 // var is a variable
 var totalTeam = tutorialTeam + editorialTeam
 totalTeam += 1
+
+let priceInferred = 19.99
+let priceExplicit: Double = 19.99
+
+let onSaleInferred = true
+let onSaleExplicit: Bool = false
+
+let nameInferred = "Whoppie Cushion"
+let nameExplicit: String = "Whoppie Cushion"
+
+if onSaleInferred {
+  println("\(nameInferred) on sale for \(priceInferred)!")
+} else {
+  println("\(nameInferred) at regular price: \(priceInferred)!")
+}
+
+// Done with variables - lets play with classes!
