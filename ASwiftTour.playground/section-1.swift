cadbf1f1b4fe83352319178f5cbc9f5605abdc1e
kenwilcox <wilcoxk@gmail.com>
2014-09-01 13:23:45 -0600

    Control Flow

--- ASwiftTour.playground/section-1.swift
+++ ASwiftTour.playground/section-1.swift
@@ -21,7 +21,7 @@ let fruitSummary = "I have \(apples + oranges) pieces of fruit."
 
 let myName = "Jose Jones"
 let debt = 14.76
-let greeting = "Hello, \(myName), you owe $\(debt)"
+let statement = "Hello, \(myName), you owe $\(debt)"
 
 // Collections
 var shoppingList = ["catfish", "water", "tulips", "blue paint"]
@@ -41,4 +41,85 @@ let emptyDictionary = [String: Float]()
 //shoppingList = []
 //occupations = [:]
 
-// Constol Flow
+// Control Flow
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+for score in individualScores {
+  if score > 50 {
+    teamScore += 3
+  } else {
+    teamScore += 1
+  }
+}
+teamScore
+
+var optionalString: String? = "Hello"
+optionalString == nil
+
+var optionalName: String? = "John Appleseed"
+optionalName = nil
+var greeting = "Hello!"
+if let name = optionalName {
+  greeting = "Hello, \(name)"
+} else {
+  // interesting - name isn't set here...
+  greeting = "Hello... and your name is?"
+}
+
+// - Switch - must contain every option (or default)
+let vegetable = "red pepper"
+switch vegetable {
+  case "celery":
+    let vegetableComment = "Add some raisins and make ants on a log."
+  case "cucumber", "watercress":
+    let vegetableComment = "That would make a good tea sandwich."
+  case let x where x.hasSuffix("pepper"):
+    let vegetableComment = "Is it a spicy \(x)"
+  default:
+    let vegetableComment = "Everything tastes good in soup."
+}
+
+// - for-in
+let interestingNumbers = [
+  "Prime":[2,3,5,7,11,13],
+  "Fibonacci": [1,1,2,3,5,8],
+  "Square": [1,4,9,16,25],
+]
+var largest = 0
+var largestKind = ""
+for (kind, numbers) in interestingNumbers {
+  for number in numbers {
+    if number > largest {
+      largest = number
+      largestKind = kind
+    }
+  }
+}
+largest
+largestKind
+
+// - while
+var n = 2
+while n < 100 {
+  n = n * 2
+}
+n
+
+var m = 2
+do {
+ m = m * 2
+} while m < 100
+m
+
+// - loops
+var firstForLoop = 0
+for i in 0..<4 {
+  firstForLoop += 1
+}
+firstForLoop
+
+var secondForLoop = 0
+for var i = 0; i < 4; ++i {
+  secondForLoop += 1
+}
+secondForLoop
