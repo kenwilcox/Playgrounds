6b12dff6e6ca3613d82bf70910ca6989c4a0f1c6
kenwilcox <wilcoxk@gmail.com>
2014-08-31 20:28:36 -0600

    Returning Tuples

--- SwiftTutorial.playground/section-1.swift
+++ SwiftTutorial.playground/section-1.swift
@@ -72,4 +72,12 @@ tipAndTotalNamed.total
 tipAndTotalNamed.0
 tipAndTotalNamed.1
 
-
+let total = 21.19
+let taxPct = 0.06
+let subtotal = total / (taxPct + 1)
+func calcTipWithTipPct(tipPct: Double) -> (tipAmt: Double, total: Double) {
+  let tipAmt = subtotal * tipPct
+  let finalTotal = total + tipAmt
+  return (tipAmt, finalTotal)
+}
+calcTipWithTipPct(0.20)
