7472a825061023e1defb33044f37c79b6ff04269
kenwilcox <wilcoxk@gmail.com>
2014-08-31 20:59:11 -0600

    Protocols can be fun

---
+++ Protocols.playground/section-1.swift
@@ -0,0 +1,47 @@
+@objc protocol Speaker {
+  func Speak()
+  optional func TellJoke()
+}
+
+class Vicki: Speaker {
+  func Speak() {
+    println("Hello, I am Vicki!")
+  }
+  func TellJoke() {
+    println("Q: What did Shshi A say to Sushi B?")
+  }
+}
+
+class Ray: Speaker {
+  func Speak() {
+    println("Yo, I am Ray!")
+  }
+  func TellJoke() {
+    println("Q: Whats the object-reiented way to becoming wealthy?")
+  }
+  func WriteTutorial() {
+    println("I'm on it!")
+  }
+}
+
+class Animal {
+  
+}
+class Dog: Animal, Speaker {
+  func Speak() {
+    println("Woof!")
+  }
+}
+
+var speaker:Speaker = Ray()
+speaker.Speak()
+//speaker.WriteTutorial() // error!
+(speaker as Ray).WriteTutorial()
+
+speaker = Vicki()
+speaker.Speak()
+// Crazy cool - don't care if the method exists
+speaker.TellJoke?()
+speaker = Dog()
+speaker.TellJoke?()
+
