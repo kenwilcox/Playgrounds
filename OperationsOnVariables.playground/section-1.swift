7a5769f58f9fdcb5f13421dcc4d5933d23266b84
kenwilcox <wilcoxk@gmail.com>
2014-09-11 22:10:02 -0600

    Strings to Int and Double

--- OperationsOnVariables.playground/section-1.swift
+++ OperationsOnVariables.playground/section-1.swift
@@ -1,4 +1,5 @@
 // Playground - noun: a place where people can play
+import Foundation // for NSString below
 
 // typealias - interesting... why?
 typealias WholeNumber = Int
@@ -67,4 +68,92 @@ println(count)
 // So it looks like countElements calls unicodeScalars on strings?
 
 let char:Character = "🐶"
+// Why would I use append over +=?
 dogString.append(char)
+dogString += "🐶"
+
+countElements(dogString)
+
+x = 5
+let newString = "\(x)"
+let floatValue = 3.5
+let newFloatString = "\(floatValue)"
+
+
+let numberString = "9"
+var numberStringToInt = numberString.toInt()!
+
+let anotherNumberString = "9.9"
+// Weird, add a ! to the folowing line and you get a crash in the
+// for codeUnit in dogString.utf8 above
+// uses a regex
+let anotherNumberStringToInt = anotherNumberString.toInt()
+
+let doubleString = "3.9585"
+// um.. why not add a .toDouble() and use a regex like above?
+var doubleValue = Double((doubleString as NSString).doubleValue)
+doubleValue += 1.1
+
+let string = NSString(string: doubleString)
+var anotherDoubleValue = string.doubleValue
+anotherDoubleValue += 1.1
+
+// With a String extension... http://stackoverflow.com/a/24102778
+extension String {
+  func toDouble() -> Double
+  {
+    let minusAscii: UInt8 = 45
+    let dotAscii: UInt8 = 46
+    let zeroAscii: UInt8 = 48
+    
+    var res = 0.0
+    let ascii = self.utf8
+    
+    var whole = [Double]()
+    var current = ascii.startIndex
+    
+    let negative = current != ascii.endIndex && ascii[current] == minusAscii
+    if (negative)
+    {
+      current = current.successor()
+    }
+    
+    while current != ascii.endIndex && ascii[current] != dotAscii
+    {
+      whole.append(Double(ascii[current] - zeroAscii))
+      current = current.successor()
+    }
+    
+    //whole number
+    var factor: Double = 1
+    for var i = countElements(whole) - 1; i >= 0; i--
+    {
+      res += Double(whole[i]) * factor
+      factor *= 10
+    }
+    
+    //mantissa
+    if current != ascii.endIndex
+    {
+      factor = 0.1
+      current = current.successor()
+      while current != ascii.endIndex
+      {
+        res += Double(ascii[current] - zeroAscii) * factor
+        factor *= 0.1
+        current = current.successor()
+      }
+    }
+    
+    if (negative)
+    {
+      res *= -1;
+    }
+    
+    return res
+  }
+}
+
+var newDoubleValue = "3.49".toDouble()
+var anotherDouble = "-0.299999".toDouble()
+var wholeNumber = "1".toDouble()
