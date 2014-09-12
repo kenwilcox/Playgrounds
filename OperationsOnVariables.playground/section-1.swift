5d4214181a641d0eef4c132c01f022acec2f0c39
kenwilcox <wilcoxk@gmail.com>
2014-09-11 21:29:21 -0600

    Playing with strings... weird

--- OperationsOnVariables.playground/section-1.swift
+++ OperationsOnVariables.playground/section-1.swift
@@ -27,3 +27,44 @@ z = 44
 // Whoo hoo!
 var 😄 = 99
 var 😢 = 😄 + z
+
+var largeNumber = 1_000_000_000_000
+
+// Strings
+var string1 = "Hello"
+var string2 = "World"
+var helloWorldString = string1 + " " + string2
+countElements(helloWorldString)
+
+let eAcuteQuestion = "Voulez-vous un caf\u{E9}?"
+let combinedEAcuteQuestion = "Voulez-vous un caf\u{65}\u{301}?"
+
+if eAcuteQuestion == combinedEAcuteQuestion {
+  println("These two strings are considered equal")
+}
+
+let latinCapitalLetterA: Character = "\u{41}"
+let cyrillicCapitalLetterA: Character = "\u{0410}"
+
+if latinCapitalLetterA != cyrillicCapitalLetterA {
+  // Ugh...
+  println("These two characters are not equivalent")
+}
+
+var dogString = "Dog‼🐶"
+countElements(dogString)
+var count = 0
+for codeUnit in dogString.utf8 {
+  count++
+}
+println(count)
+
+count = 0
+for scalar in dogString.unicodeScalars {
+  count++
+}
+println(count)
+// So it looks like countElements calls unicodeScalars on strings?
+
+let char:Character = "🐶"
+dogString.append(char)
