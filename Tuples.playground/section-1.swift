0674e2e6dfe42a7667dde2f16fcc584247708763
kenwilcox <wilcoxk@gmail.com>
2014-12-20 21:06:50 -0700

    Playing with Tuples

---
+++ Tuples.playground/section-1.swift
@@ -0,0 +1,14 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var myTuple = ("Tiger", "Bengal", 3)
+let name = myTuple.0
+
+
+var secondTigerTuple = (name: "Tigress", breed: "Indochinese Tiger", age: 3)
+
+let age = secondTigerTuple.age
+let ageAgain = secondTigerTuple.2
+
+
