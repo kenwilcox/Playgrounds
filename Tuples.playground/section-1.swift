099c9004f1e9ca3088266626b020aa6ecab927f9
kenwilcox <wilcoxk@gmail.com>
2014-12-20 21:12:07 -0700

    Tuple switch

--- Tuples.playground/section-1.swift
+++ Tuples.playground/section-1.swift
@@ -11,4 +11,13 @@ var secondTigerTuple = (name: "Tigress", breed: "Indochinese Tiger", age: 3)
 let age = secondTigerTuple.age
 let ageAgain = secondTigerTuple.2
 
-
+// Switch
+switch secondTigerTuple {
+case ("Tigress", "Malayan", 2):
+  println("first case is true")
+case (_, "Indochinese Tiger", _):
+  // I like this!
+  println("second case is true")
+default:
+  println("default is occuring")
+}
