2ef6da1ba9b0df85b9f7662c92cb8dec3dd60d33
kenwilcox <wilcoxk@gmail.com>
2014-12-26 11:10:51 -0700

    playing with rand()

---
+++ MoreRandom.playground/section-1.swift
@@ -0,0 +1,25 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+// Trying to see how rand() works
+
+var delay = NSTimeInterval(((Float(rand()) % 900.0) + 100.0) / 1000.0)
+delay = NSTimeInterval(((Float(rand()) % 900.0) + 100.0) / 1000.0)
+delay = NSTimeInterval(((Float(rand()) % 900.0) + 100.0) / 1000.0)
+delay = NSTimeInterval(((Float(rand()) % 900.0) + 100.0) / 1000.0)
+delay = NSTimeInterval(((Float(rand()) % 900.0) + 100.0) / 1000.0)
+
+let size : CGFloat = CGFloat( Int(rand()) %  40 + 20)
+var yPosition : CGFloat = CGFloat( Int(rand()) %  Int(667.0 * 0.5) + 20)
+yPosition = CGFloat( Int(rand()) %  Int(667.0 * 0.5) + 20)
+yPosition = CGFloat( Int(rand()) %  Int(667.0 * 0.5) + 20)
+yPosition = CGFloat( Int(rand()) %  Int(667.0 * 0.5) + 20)
+yPosition = CGFloat( Int(rand()) %  Int(667.0 * 0.5) + 20)
+yPosition = CGFloat( Int(rand()) %  Int(667.0 * 0.5) + 20)
+yPosition = CGFloat( Int(rand()) %  Int(667.0 * 0.5) + 20)
+
+yPosition - (size * 2) - yPosition
+
+
+
