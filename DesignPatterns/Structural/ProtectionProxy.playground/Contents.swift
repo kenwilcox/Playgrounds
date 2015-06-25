9c05d34dbe53e1e1160a0d13844fed9e3a1244fc
Ken Wilcox <wilcoxk@gmail.com>
2015-06-24 19:52:28 -0600

    removed UIKit import

--- DesignPatterns/Structural/ProtectionProxy.playground/Contents.swift
+++ DesignPatterns/Structural/ProtectionProxy.playground/Contents.swift
@@ -1,7 +1,5 @@
 //: Protection Proxy - The proxy pattern is used to provide a surrogate or placeholder object, which references an underlying object. Protection proxy is restricting access.
 
-import UIKit
-
 protocol DoorOperator {
   func openDoors(doors: String) -> String
 }
